f22d062e0a0a4f32c3dc849e3551dbfb5a8bb6d4
Idiaghe Osaigbovo <idiagheosaigbovoosas@gmail.com>
2020-11-14 23:08:15 +0100

    days task

---
+++ Es6/song.js
@@ -0,0 +1,41 @@
+//Assignment of the day
+
+var artiest = "JayZ";
+
+//string was used in this first one 
+
+var genre = "Hip" + "hop";
+
+//string was used with cantation.
+
+var songDuration = {
+    firstSong: "23",
+    songName: "money",
+    secondSong: "day",
+    duration: "4"
+};
+
+//object was used here.
+
+var yearRelased = "2019";
+
+//numbers was used.
+
+var albumList = ["money", "day", "light"];
+
+//creatting with arrarys 
+
+var newAlbum = new Boolean (true);
+
+//one with boolean.
+
+//console 
+
+console.log(artiest);
+console.log(genre);
+console.log(songDuration);
+console.log(yearRelased);
+console.log(albumList);
+console.log(newAlbum);
+
+
